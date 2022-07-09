a2b14e85886a718ba1221ee1e923e49be0295d41
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-09 20:47:49 +0530

    using data updated by api to render dynamic content, (and understanding react lifecycle a bit)

--- zomato-app/src/component/Home/searchComponent.js
+++ zomato-app/src/component/Home/searchComponent.js
@@ -11,6 +11,26 @@ class Search extends Component {
             location: ""
         }
     }
+
+    renderCity = (data) => {
+        // important to use this if condition here, because 
+        // page load: constructor -> render -> componentDidMount
+        // so it gets called before componentDidMount(), and then we had this.state.location as "" (i.e. empty)
+        // since the api (inside componentDidMount() )is not called till this time
+        // so using if(data is there), saves us from the error: TypeError: data.map is not a function
+        
+        // so when componentDidMount() is called, then this.state.location gets updated
+        // and then component is then re-rendered with updated data, and this function runs well now.
+        if (data) {
+            return data.map((item) => {
+                return (
+                    <li key={item._id}><a href="/" className="dropdown-item">{item.state}</a></li>
+                    // <li key={item.id}><a href="/" className="dropdown-item">{item.state}</a></li>
+                )
+            })
+        }
+    }
+
     render() {
         return (
             <>
@@ -26,10 +46,11 @@ class Search extends Component {
                         <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown">----Select Your
                             City----</button>
                         <ul className="dropdown-menu">
-                            <li><a href="/" className="dropdown-item">Mathura</a></li>
+                            {this.renderCity(this.state.location)}
+                            {/* <li><a href="/" className="dropdown-item">Mathura</a></li>
                             <li><a href="/" className="dropdown-item">Agra</a></li>
                             <li><a href="/" className="dropdown-item">Mumbai</a></li>
-                            <li><a href="/" className="dropdown-item">Delhi</a></li>
+                            <li><a href="/" className="dropdown-item">Delhi</a></li> */}
                         </ul>
 
                         <button type="button" className="btn dropdown-toggle" data-bs-toggle="dropdown">----Select Your
@@ -49,17 +70,22 @@ class Search extends Component {
         // if we need to make API call on page load, we do it here
 
         // this method gets called when page loads.
-        // page load: constructor() -> super() -> then out of constructor: render() -> componentDidMount()
-        
+        // page load (only once): constructor() -> render() -> componentDidMount()
+
+        // first time, page loads; component is mounted, then it only re-renders
+        // the constructor or componentDidMount is not called anymore,
+        // any state change happens, then only render() function runs again (with updated data)
+        // i.e. only re-rendering.
+
         // we get data from an API, & that we update in a state, & as soon any state changes,
         // the component re-renders & we get the updated data rendered on page.
 
         // making api call
-        fetch(url, {method:'GET'})
-        .then((res) => res.json())  
-        .then((data) => (
-            this.setState({location: data})
-        ))
+        fetch(url, { method: 'GET' })
+            .then((res) => res.json())
+            .then((data) => (
+                this.setState({ location: data })
+            ))
     }
 }
 
