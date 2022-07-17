395aa44b3f31d01dadd67c476e63819ac8af22cc
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-17 17:22:26 +0530

    tweaks to footer

--- zomato-app/src/footer.js
+++ zomato-app/src/footer.js
@@ -5,22 +5,10 @@ const Footer = () => {
     return (
         <footer>
             <div className="container-fluid text-center">
-
+                <div class="footer-heading">
+                    <h2>Connect With Me</h2>
+                </div>
                 <div className="footer-socials" style={{ fontSize: '1rem', textAlign: 'center', padding: '1%' }}>
-                    <a href="https://facebook.com" target="_blank" rel="noreferrer">
-                        <i className="bi bi-facebook" style={{ color: 'blue' }}></i>
-                    </a>
-                    <a href="https://instagram.com/my_emerging_thoughts" target="_blank" rel="noreferrer">
-                        <i className="bi bi-instagram" style={{ color: 'red' }}></i>
-                    </a>
-                    <a href="https://twitter.com" target="_blank" rel="noreferrer">
-                        <i className="bi bi-twitter" style={{ color: 'lightblue' }}></i>
-                    </a>
-
-                    <a href="https://youtube.com" target="_blank" rel="noreferrer">
-                        <i className="bi bi-youtube" style={{ color: 'red' }}></i>
-                    </a>
-
                     <a href="https://github.com/suraj-singh12" target="_blank" rel="noreferrer">
                         <i className="bi bi-github" style={{color: 'black'}}></i>
                     </a>
@@ -28,6 +16,20 @@ const Footer = () => {
                     <a href="https://linkedin.com" target="_blank" rel="noreferrer">
                         <i className="bi bi-linkedin" style={{ color: 'blue' }}></i>
                     </a>
+                    <a href="https://twitter.com/suraj_singh_12" target="_blank" rel="noreferrer">
+                        <i className="bi bi-twitter" style={{ color: 'lightblue' }}></i>
+                    </a>
+
+                    <a href="https://youtube.com" target="_blank" rel="noreferrer">
+                        <i className="bi bi-youtube" style={{ color: 'red' }}></i>
+                    </a>
+                    <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                        <i className="bi bi-facebook" style={{ color: 'blue' }}></i>
+                    </a>
+                    <a href="https://instagram.com/my_emerging_thoughts" target="_blank" rel="noreferrer">
+                        <i className="bi bi-instagram" style={{ color: 'red' }}></i>
+                    </a>
+
 
                 </div>
 
